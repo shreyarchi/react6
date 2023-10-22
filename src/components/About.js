b8a3a1068038ce5cef7a0cc2633ffca2e9b57f11
shreyarchi <128414539+shreyarchi@users.noreply.github.com>
2023-10-22 23:26:36 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Hi, i am Shreya</p>
+    </section>
+  );
+};
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,27 @@
+import React from "react";
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+
+      <ul>
+        <li>
+          EmailId:<a href="#">Email</a>
+        </li>
+        <li>
+          Linkedin:
+          <a href="#" target="_blank">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <strong>GitHub:</strong>{" "}
+          <a href=" " target="_blank" rel="noopener noreferrer">
+            github
+          </a>{" "}
+        </li>
+      </ul>
+    </section>
+  );
+};
+export default Contact;
